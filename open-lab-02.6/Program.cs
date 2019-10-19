309ce954538b40971f8fd52c9a1ccaf556ed8f86
martinspilak <martinecko24@gmail.com>
2019-10-19 12:56:44 +0200

    open-lab-02.6

---
+++ open-lab-02.6/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace open_lab_02._6
+{
+    class Program
+    {
+        public static int Sum(int a)
+        {
+            return a % 100;
+        }
+        static void Main(string[] args)
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            if (a % 100 == 0)
+            {
+                Console.WriteLine(true);
+            }
+            else
+            {
+                Console.WriteLine(false);
+            }
+        }
+    }
+}
